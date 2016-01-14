2c034ac6146fd0cc0ea521b48d5f97638505b9fc
Azizur Rahman <azizur.ice@gmail.com>
2016-01-13 16:28:50 -0800

    Implemented UI

--- src/com/addictiveads/restclient/MainActivity.java
+++ src/com/addictiveads/restclient/MainActivity.java
@@ -6,13 +6,37 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    protected Button mSendButton;
+    protected TextView mServerResponse;
+    protected EditText mYourName;
+
+    protected final String BASE_URL = "http://dev.addictiveadsnetwork.com/api/v1/test/hello/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mServerResponse = (TextView) findViewById(R.id.text_server_response);
+        mYourName = (EditText) findViewById(R.id.edit_name);
+        mSendButton = (Button) findViewById(R.id.button_send);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String name = mYourName.getText().toString();
+                mServerResponse.setText(name);
+
+                // here call the class which has implemented AsyncTask with URL to access Web Service
+            }
+        });
     }
 
     @Override
